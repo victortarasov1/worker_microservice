56d89099013975bb06b5d03728433930fca395eb
Seroja <sersergey04@gmail.com>
2023-05-13 14:33:36 +0300

    Updated dependencies

--- executor-service/src/test/java/executor/service/AppTest.java
+++ executor-service/src/test/java/executor/service/AppTest.java
@@ -1,8 +1,8 @@
 package executor.service;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
@@ -13,8 +13,7 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        Assertions.assertTrue( true );
     }
 }
