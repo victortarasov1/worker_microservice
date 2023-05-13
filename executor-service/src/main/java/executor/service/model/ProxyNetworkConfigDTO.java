65c565e39eb2ef176fc3711b312fe5d7ffff664c
Katya <ksemeukina@gmail.com>
2023-05-13 17:06:55 +0200

    Create class ProxyNetworkConfigDTO

---
+++ executor-service/src/main/java/executor/service/model/ProxyNetworkConfigDTO.java
@@ -0,0 +1,48 @@
+package executor.service.model;
+
+import java.util.Objects;
+
+public class ProxyNetworkConfigDTO {
+    String hostname;
+    Integer port;
+
+    public ProxyNetworkConfigDTO() {
+    }
+
+    public ProxyNetworkConfigDTO(String hostname, Integer port) {
+        this.hostname = hostname;
+        this.port = port;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        ProxyNetworkConfigDTO that = (ProxyNetworkConfigDTO) o;
+        return Objects.equals(hostname, that.hostname)
+                && Objects.equals(port, that.port);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(hostname, port);
+    }
+}
