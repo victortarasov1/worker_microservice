652f7aa126c525cae5c42a7253d40f104bb99868
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-05-20 22:21:06 +0300

    added javadoc

--- executor-service/src/main/java/executor/service/factory/difactory/DependencyInjectionFactory.java
+++ executor-service/src/main/java/executor/service/factory/difactory/DependencyInjectionFactory.java
@@ -1,7 +1,17 @@
 package executor.service.factory.difactory;
 
+/**
+ * A factory interface for creating instances of classes based on their interface.
+ */
 public interface DependencyInjectionFactory {
 
+    /**
+     * Creates an instance of a class that implements the specified interface.
+     *
+     * @param interfaceClass the interface class for which to create an instance
+     * @param <T>            the type of the interface
+     * @return an instance of the class that implements the interface
+     */
     <T> T createInstance(Class<T> interfaceClass);
 
 }
