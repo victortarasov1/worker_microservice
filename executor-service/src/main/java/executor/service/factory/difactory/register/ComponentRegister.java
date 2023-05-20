1d7c2db6a6f1d820b48e746176f4937d884d00c2
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-05-20 22:49:23 +0300

    created InstanceRegister interface and its implementations

---
+++ executor-service/src/main/java/executor/service/factory/difactory/register/ComponentRegister.java
@@ -0,0 +1,52 @@
+package executor.service.factory.difactory.register;
+
+import executor.service.annotation.Component;
+import executor.service.expetion.ConstructorNotFoundException;
+import executor.service.expetion.InstanceCreationException;
+import executor.service.factory.difactory.DependencyInjectionFactory;
+import executor.service.factory.difactory.ServiceRegisterFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class ComponentRegister implements InstanceRegister {
+    private InstanceRegister nextRegister;
+    private final String packageName;
+
+    public ComponentRegister(String packageName) {
+        this.packageName = packageName;
+    }
+    @Override
+    public void register(Class<?> clazz) {
+        if (clazz.getDeclaredAnnotation(Component.class) != null) registerComponent(clazz);
+        else if (nextRegister != null) nextRegister.register(clazz);
+    }
+
+    @Override
+    public void setNextRegister(InstanceRegister nextRegister) {
+        this.nextRegister = nextRegister;
+    }
+    private void registerComponent(Class<?> clazz) {
+        Predicate<Class<?>> isDesiredInterface = interfaceClazz
+                -> interfaceClazz.getPackage().getName().startsWith(packageName);
+        Arrays.stream(clazz.getInterfaces()).filter(isDesiredInterface)
+                .forEach(interfaceClazz -> ServiceRegisterFactory.register(interfaceClazz, getInstance(clazz)));
+    }
+
+    private Function<DependencyInjectionFactory, Object> getInstance(Class<?> clazz) {
+        return (serviceFactory) -> {
+            try {
+                Constructor<?> constructor = Arrays.stream(clazz.getDeclaredConstructors())
+                        .findFirst().orElseThrow(() -> new ConstructorNotFoundException(clazz));
+                Object[] objects = Arrays.stream(constructor.getParameters()).map(Parameter::getType).map(serviceFactory::createInstance).toArray();
+                return constructor.newInstance(objects);
+            } catch (InvocationTargetException | InstantiationException | IllegalAccessException | IllegalArgumentException ex) {
+                throw new InstanceCreationException(ex.getMessage(), clazz);
+            }
+        };
+    }
+}
---
+++ executor-service/src/main/java/executor/service/factory/difactory/register/ConfigRegister.java
@@ -0,0 +1,49 @@
+package executor.service.factory.difactory.register;
+
+import executor.service.annotation.Bean;
+import executor.service.annotation.Config;
+import executor.service.expetion.ConfigurationRegistrationException;
+import executor.service.expetion.MethodInvocationException;
+import executor.service.factory.difactory.DependencyInjectionFactory;
+import executor.service.factory.difactory.ServiceRegisterFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.function.Function;
+
+public class ConfigRegister implements InstanceRegister {
+    private InstanceRegister nextRegister;
+
+    @Override
+    public void register(Class<?> clazz) {
+        if(clazz.getDeclaredAnnotation(Config.class) != null) registerConfig(clazz);
+        else if(nextRegister != null) nextRegister.register(clazz);
+    }
+
+    @Override
+    public void setNextRegister(InstanceRegister nextRegister) {
+        this.nextRegister = nextRegister;
+    }
+
+    private void registerConfig(Class<?> clazz) {
+        try {
+            Object instance = clazz.getDeclaredConstructor().newInstance();
+            Arrays.stream(clazz.getMethods()).filter(v -> v.isAnnotationPresent(Bean.class))
+                    .forEach(v -> ServiceRegisterFactory.register(v.getReturnType(), getInstance(instance, v)));
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new ConfigurationRegistrationException(e.getMessage(), clazz);
+        }
+    }
+
+    private Function<DependencyInjectionFactory, Object> getInstance(Object instance, Method method) {
+        return (factory) -> {
+            try {
+                return method.invoke(instance);
+            } catch (IllegalAccessException | InvocationTargetException | IllegalArgumentException e) {
+                throw new MethodInvocationException(e.getMessage(), instance.getClass());
+            }
+        };
+    }
+}
