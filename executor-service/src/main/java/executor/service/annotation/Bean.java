4c69dc2763f014ade2779022a28b80d0240da777
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-05-21 14:23:11 +0300

    added javadoc

--- executor-service/src/main/java/executor/service/annotation/Bean.java
+++ executor-service/src/main/java/executor/service/annotation/Bean.java
@@ -5,6 +5,22 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * The {@code @Bean} annotation is used to annotate methods in a class annotated with {@code @Config},
+ * which are intended to return instances of interfaces for the dependency injection factory.
+ * <p>
+ * The annotated method should be public, have no parameters, and return an interface rather than its implementation.
+ * If these conditions are not met, a {@code MethodInvocationException} will be thrown.
+ * <p>
+ * For example:
+ * {@code @Bean}
+ * {@code public SomeInterface someBean() {
+ * return new SomeInterfaceImpl(dependencies);
+ * }}
+ * <p>
+ * In this example, the {@code someBean()} method returns an instance of the {@code SomeInterface} interface.
+ * This method will be called by the dependency injection factory to provide the implementation for dependency injection.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Bean {
--- executor-service/src/main/java/executor/service/annotation/Config.java
+++ executor-service/src/main/java/executor/service/annotation/Config.java
@@ -5,6 +5,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * The {@code @Config} annotation is used to allow the dependency injection factory to create implementations of
+ * your interfaces with specific parameters, such as primitive types, wrapper classes, collections, and so on.
+ * <p>
+ * The class annotated with {@code @Config} should have a public default constructor.
+ * This allows the dependency injection factory to instantiate the configuration and use it for creating instances.
+ * If a public default constructor is not found, a {@code ConfigurationRegistrationException} will be thrown.
+ * <p>
+ * To specify which implementations of your interfaces should be injected by the factory, you can use the {@code @Bean} annotation.
+ * By annotating a method with {@code @Bean} inside the class annotated with {@code @Config}, you can indicate which implementations
+ * of your interfaces should be used and how they should be configured.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Component
