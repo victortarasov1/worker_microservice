1d6afe18b0b184ccd4d94a0ebad3133e826838a1
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-06-21 13:26:36 +0300

    wrote tests for db appender

---
+++ executor-service/src/test/java/executor/service/appender/DataBaseAppenderTest.java
@@ -0,0 +1,91 @@
+package executor.service.appender;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.IThrowableProxy;
+import ch.qos.logback.classic.spi.StackTraceElementProxy;
+import executor.service.appender.connection.DBConnection;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class DataBaseAppenderTest {
+
+    private DBConnection mockConnection;
+
+
+    private ILoggingEvent mockLoggingEvent;
+
+
+    private IThrowableProxy mockThrowableProxy;
+
+    private DataBaseAppender appender;
+
+    @BeforeEach
+    public void setUp() {
+        mockConnection = Mockito.mock(DBConnection.class);
+        mockLoggingEvent = Mockito.mock(ILoggingEvent.class);
+        mockThrowableProxy = Mockito.mock(IThrowableProxy.class);
+        appender = new DataBaseAppender();
+        appender.setConnection(mockConnection);
+    }
+
+    @Test
+    public void testStart_ConnectToDb() {
+        System.out.println();
+        appender.start();
+        verify(mockConnection).connect();
+    }
+
+    @Test
+    public void testStop_DisconnectFromDb() {
+        appender.stop();
+        verify(mockConnection).disconnect();
+    }
+
+    @Test
+    public void testAppend_SaveEventToDb() {
+        configureMockLoggingEvent();
+        when(mockLoggingEvent.getLevel()).thenReturn(Level.INFO);
+        appender.append(mockLoggingEvent);
+        verify(mockConnection).saveLogEvent(anyString(), anyString(),anyString(), anyString(), anyLong(), anyString(), anyString(),anyString(),anyString());
+    }
+
+    @Test
+    public void testAppend_SaveEventWithStackTraceToDb() {
+        configureMockLoggingEvent();
+        when(mockLoggingEvent.getLevel()).thenReturn(Level.ERROR);
+        when(mockLoggingEvent.getThrowableProxy()).thenReturn(mockThrowableProxy);
+
+        StackTraceElementProxy[] stackTraceElementProxies = new StackTraceElementProxy[]{
+                new StackTraceElementProxy(
+                        new StackTraceElement("ExceptionClass", "ExceptionMethod", "ExceptionFilename", 456)
+                ),
+                new StackTraceElementProxy(
+                        new StackTraceElement("ExceptionClass", "ExceptionMethod", "ExceptionFilename", 457)
+                )
+        };
+        when(mockThrowableProxy.getClassName()).thenReturn("ExceptionClass");
+        when(mockThrowableProxy.getStackTraceElementProxyArray()).thenReturn(stackTraceElementProxies);
+
+        appender.append(mockLoggingEvent);
+        verify(mockConnection).saveLogEvent(anyString(), anyString(),anyString(), anyString(), anyLong(), anyString(), anyString(),anyString(),anyString());
+        verify(mockConnection).saveExceptionStackTrace(anyLong(), anyList());
+
+    }
+
+    private void configureMockLoggingEvent() {
+        when(mockLoggingEvent.getFormattedMessage()).thenReturn("Test error message");
+        when(mockLoggingEvent.getLoggerName()).thenReturn("TestLogger");
+        when(mockLoggingEvent.getThreadName()).thenReturn("TestThread");
+        when(mockLoggingEvent.getTimeStamp()).thenReturn(987654321L);
+        StackTraceElement[] callerData = new StackTraceElement[]{
+                new StackTraceElement("TestCallerClass", "TestCallerMethod", "TestCallerFilename", 123)
+        };
+        when(mockLoggingEvent.getCallerData()).thenReturn(callerData);
+    }
+}
