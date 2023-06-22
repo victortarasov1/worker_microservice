405b777688430342e1affb2fb9649ddc3a0d8188
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-06-22 23:04:49 +0300

    renamed

--- executor-service/src/main/java/executor/service/appender/manager/LogDatabaseManager.java
+++ executor-service/src/main/java/executor/service/appender/manager/LogDatabaseManager.java
@@ -2,43 +2,113 @@ package executor.service.appender.manager;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.classic.spi.IThrowableProxy;
-import executor.service.exception.dbmanagerexception.ConnectionFailedException;
-import executor.service.exception.dbmanagerexception.DisconnectionFailedException;
-import executor.service.exception.dbmanagerexception.LogEventSaveException;
-import executor.service.exception.dbmanagerexception.StackTraceSaveException;
+import ch.qos.logback.classic.spi.StackTraceElementProxy;
+import executor.service.exception.logstorage.ConnectionFailedException;
+import executor.service.exception.logstorage.DisconnectionFailedException;
+import executor.service.exception.logstorage.LogEventSaveException;
+import executor.service.exception.logstorage.StackTraceSaveException;
 
-public interface LogDatabaseManager {
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    /**
-     * Connects to the database.
-     *
-     * @throws ConnectionFailedException if failed to connect to the database
-     */
-    void connect();
+public class LogDatabaseManager implements LogStorageManager {
+    private String url;
+    private String username;
+    private String password;
+    private Connection connection;
 
-    /**
-     * Disconnects from the database.
-     *
-     * @throws DisconnectionFailedException if failed to disconnect from the database
-     */
-    void disconnect();
+    @Override
+    public void connect() {
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException ex) {
+            throw new ConnectionFailedException(ex);
+        }
+    }
 
-    /**
-     * Saves a log event to the database.
-     *
-     * @param event the log event to be saved
-     * @return the ID of the saved log event
-     * @throws LogEventSaveException if failed to save the log event to the database
-     */
-    long saveLogEvent(ILoggingEvent event);
+    @Override
+    public void disconnect() {
+        try {
+            connection.close();
+        } catch (SQLException ex) {
+            throw new DisconnectionFailedException(ex);
+        }
+    }
 
-    /**
-     * Saves an exception stack trace to the database.
-     *
-     * @param eventId        the ID of the corresponding log event
-     * @param throwableProxy the throwable proxy representing the exception stack trace
-     * @throws StackTraceSaveException if failed to save the exception stack trace to the database
-     */
-    void saveExceptionStackTrace(long eventId, IThrowableProxy throwableProxy);
+    @Override
+    public long saveLogEvent(ILoggingEvent event) {
+        String sql = """
+                INSERT INTO logging_event
+                (timestmp, formatted_message, logger_name, level_string,
+                thread_name, caller_filename, caller_class, caller_method,
+                caller_line) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                """;
+        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setLong(1, event.getTimeStamp());
+            statement.setString(2, event.getFormattedMessage());
+            statement.setString(3, event.getLoggerName());
+            statement.setString(4, event.getLevel().toString());
+            statement.setString(5, event.getThreadName());
+            statement.setString(6, event.getCallerData()[0].getFileName());
+            statement.setString(7, event.getCallerData()[0].getClassName());
+            statement.setString(8, event.getCallerData()[0].getMethodName());
+            statement.setString(9, String.valueOf(event.getCallerData()[0].getLineNumber()));
+            statement.executeUpdate();
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            generatedKeys.next();
+            return generatedKeys.getLong(1);
+        } catch (SQLException ex) {
+            throw new LogEventSaveException(ex);
+        }
+    }
+    @Override
+    public void saveExceptionStackTrace(long eventId, IThrowableProxy throwableProxy) {
+        List<String> traceLines = getThrowableProxyTraceLines(throwableProxy);
+        String sql = """
+                INSERT INTO logging_event_exception
+                (event_id, i, trace_line)
+                VALUES (?, ?, ?)
+                """;
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            for (int i = 0; i < traceLines.size(); i++) {
+                statement.setLong(1, eventId);
+                statement.setInt(2, i);
+                statement.setString(3, traceLines.get(i));
+                statement.addBatch();
+            }
+            statement.executeBatch();
+        } catch (SQLException ex) {
+            throw new StackTraceSaveException(ex);
+        }
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    private List<String> getThrowableProxyTraceLines(IThrowableProxy throwableProxy) {
+        List<String> traceLines = new ArrayList<>();
+        traceLines.add(throwableProxy.getClassName() + ": " + throwableProxy.getMessage());
+        getFormattedStackTraceElements(throwableProxy);
+        throwableProxy = throwableProxy.getCause();
+        while (throwableProxy != null) {
+            traceLines.add("Caused by: " + throwableProxy.getClassName() + ": " + throwableProxy.getMessage());
+            traceLines.addAll(getFormattedStackTraceElements(throwableProxy));
+            throwableProxy = throwableProxy.getCause();
+        }
+        return traceLines;
+    }
+    private List<String> getFormattedStackTraceElements(IThrowableProxy throwableProxy) {
+        return Arrays.stream(throwableProxy.getStackTraceElementProxyArray())
+                .map(StackTraceElementProxy::getStackTraceElement)
+                .map(v -> "at " + v).toList();
+    }
 }
