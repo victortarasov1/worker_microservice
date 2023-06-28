caf14717fab931f077cda521e4d4e67c95da5b3f
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-06-28 13:35:42 +0300

    wrote logging proxy

---
+++ executor-service/src/main/java/executor/service/proxy/LoggingProxyProvider.java
@@ -0,0 +1,19 @@
+package executor.service.proxy;
+
+import org.slf4j.Logger;
+import java.lang.reflect.Proxy;
+
+public class LoggingProxyProvider {
+    private LoggingProxyProvider() {
+
+    }
+
+    public static <T> T createProxy(T target, Class<T> tInterface, Logger logger) {
+        var handler = new LoggingInvocationHandler<>(target, logger);
+        return tInterface.cast(Proxy.newProxyInstance(
+                target.getClass().getClassLoader(),
+                target.getClass().getInterfaces(),
+                handler)
+        );
+    }
+}
