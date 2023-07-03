90541056ee43a4e37213447542323e3c67d884c8
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2023-07-03 21:33:36 +0300

    Refactored ExecutionService class With the filling of its facade logic

--- executor-service/src/main/java/executor/service/ExecutionService.java
+++ executor-service/src/main/java/executor/service/ExecutionService.java
@@ -1,9 +1,21 @@
 package executor.service;
 
+import executor.service.model.ScenarioDto;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Optional;
+
 public class ExecutionService {
 
-    public void execute(ScenarioSourceListener scenarioSourceListener) {
-        scenarioSourceListener.execute();
+    public void execute(WebDriver webDriver, ScenarioSourceListener scenarioSourceListener,
+                        ScenarioExecutor scenarioExecutor) {
+
+        Optional<ScenarioDto> scenario = scenarioSourceListener.getScenario();
+
+        while(scenario.isPresent()) {
+            scenarioExecutor.execute(scenario.get(), webDriver);
+            scenario = scenarioSourceListener.getScenario();
+        }
     }
 
 }
