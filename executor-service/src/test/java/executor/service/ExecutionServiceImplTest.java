0a2ff0c15257704f0b0ad8fceae32eee8b823dcd
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2023-07-03 22:01:11 +0300

    Removed all tests for previous version of facade With perspective of creating new tests

--- executor-service/src/test/java/executor/service/ExecutionServiceImplTest.java
+++ executor-service/src/test/java/executor/service/ExecutionServiceImplTest.java
@@ -1,29 +1,6 @@
 package executor.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+class ExecutionServiceImplTest {
 
-class ExecutionServiceTest {
-
-    private static final ExecutionService EXECUTION_SERVICE = Mockito.mock(ExecutionService.class);
-
-    private static final ScenarioSourceListener SCENARIO_SOURCE_LISTENER = Mockito.mock(ScenarioSourceListener.class);
-
-
-    @Test
-    void successfulExecutionTest() {
-        EXECUTION_SERVICE.execute(SCENARIO_SOURCE_LISTENER);
-        Mockito.verify(EXECUTION_SERVICE, Mockito.times(1)).execute(SCENARIO_SOURCE_LISTENER);
-    }
-
-    @Test
-    void failureExecutionTest() {
-        String errorMessage = "Execution failed";
-        
-        Mockito.doThrow(new RuntimeException(errorMessage)).when(EXECUTION_SERVICE).execute(SCENARIO_SOURCE_LISTENER);
-
-        Assertions.assertThrows(RuntimeException.class, () -> EXECUTION_SERVICE.execute(SCENARIO_SOURCE_LISTENER), errorMessage);
-    }
 
 }
