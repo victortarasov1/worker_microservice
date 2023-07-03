5b5890baa211ec702f0a34c3d002bfbcc31d47b0
Savchenko Yaroslav <savchenko.yrslv@gmail.com>
2023-07-03 20:11:11 +0300

    JsonScenarioSource refactoring

--- executor-service/src/main/java/executor/service/JsonScenarioSource.java
+++ executor-service/src/main/java/executor/service/JsonScenarioSource.java
@@ -1,18 +1,12 @@
 package executor.service;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import executor.service.exception.ResourceFileNotFoundException;
+import executor.service.annotation.Component;
 import executor.service.model.ScenarioDto;
+import executor.service.utl.JsonReader;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.Objects;
 
+@Component
 public class JsonScenarioSource implements ScenarioSource {
     private String resourceName = "scenarios.json";
 
@@ -23,26 +17,6 @@ public class JsonScenarioSource implements ScenarioSource {
     }
 
     public List<ScenarioDto> getScenarios() {
-        ObjectMapper mapper = new ObjectMapper();
-
-        try {
-            return mapper.readValue(getFile(), new TypeReference<>() {});
-        } catch (IOException e) {
-            throw new RuntimeException(e.getMessage());
-        } catch (NullPointerException e) {
-            throw new ResourceFileNotFoundException(resourceName);
-        }
-    }
-
-    private File getFile() {
-        Path resourcePath = Paths.get(resourceName);
-
-        if (resourcePath.isAbsolute()) {
-            return resourcePath.toFile();
-        }
-
-        URL url = Objects.requireNonNull(this.getClass().getClassLoader().getResource(resourceName));
-
-        return new File(url.getFile());
+        return JsonReader.parseResourceToList(resourceName, ScenarioDto.class);
     }
 }
