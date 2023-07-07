5c0eaa4aafc280a6aef14d2ac579b3f1afa90e8d
Katya <ksemeukina@gmail.com>
2023-07-07 15:37:54 +0200

    Fourth refactoring

--- executor-service/src/test/java/executor/service/ParallelFlowExecutorImplTest.java
+++ executor-service/src/test/java/executor/service/ParallelFlowExecutorImplTest.java
@@ -44,12 +44,13 @@ class ParallelFlowExecutorImplTest {
         when(proxySourcesClient.getProxy()).thenReturn(proxy);
         when(threadPoolConfigDto.getCorePoolSize()).thenReturn(NUMBER_OF_THREADS);
         when(driverProvider.create(proxy)).thenReturn(driver);
-        doNothing().when(scenarioSourceListener).execute();
+
+        scenarioSourceListener.execute();
 
         parallelFlowExecutor.runInParallelFlow();
 
-//        verify(executionService, times(NUMBER_OF_THREADS))
-//                .execute(driver, scenarioSourceListener, scenarioExecutor);
+        verify(executionService, times(NUMBER_OF_THREADS))
+                .execute(driver, scenarioSourceListener, scenarioExecutor);
 
         verify(proxySourcesClient, times(NUMBER_OF_GET_PROXY_CALL)).getProxy();
 
