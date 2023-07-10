79fb8703df35ba981f2e1471e6c72401c7b0f408
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-07-10 21:12:18 +0300

    removed unused imports

--- executor-service/src/main/java/executor/service/config/CustomConfiguration.java
+++ executor-service/src/main/java/executor/service/config/CustomConfiguration.java
@@ -1,6 +1,4 @@
 package executor.service.config;
-import executor.service.ScenarioExecutor;
-import executor.service.ScenarioExecutorImpl;
 import executor.service.annotation.Bean;
 import executor.service.annotation.Config;
 import executor.service.maintenance.plugin.proxy.JsonProxySources;
@@ -8,16 +6,8 @@ import executor.service.maintenance.plugin.proxy.ProxySourcesClient;
 import executor.service.maintenance.plugin.proxy.ProxySourcesClientImpl;
 import executor.service.model.ThreadPoolConfigDto;
 import executor.service.model.WebDriverConfigDto;
-import executor.service.logger.LoggingProxyProvider;
-import executor.service.stepexecution.ClickCss;
-import executor.service.stepexecution.ClickXpath;
-import executor.service.stepexecution.Sleep;
-import executor.service.stepexecution.StepExecution;
 import executor.service.utl.PropertyReader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.util.List;
 
 @Config
 public class CustomConfiguration {
