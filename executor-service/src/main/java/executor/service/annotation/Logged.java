edc45bfb3d65fb2faa9bad43a1a277daa7746ecd
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-07-10 21:33:15 +0300

    wrote javadoc

--- executor-service/src/main/java/executor/service/annotation/Logged.java
+++ executor-service/src/main/java/executor/service/annotation/Logged.java
@@ -5,6 +5,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Indicates that all methods, including constructors, within classes
+ * annotated with this annotation will be logged.
+ * Logging includes the start of method execution, which logs the
+ * method name, class name, and method arguments,
+ * and the end of method execution, which logs the method name and
+ * class name.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Logged {
