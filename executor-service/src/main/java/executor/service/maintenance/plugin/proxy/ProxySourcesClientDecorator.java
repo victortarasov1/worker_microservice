62d83618f991bc1a743041032ac3ffaf864c43c5
ilwsm <ua.andrey@rocketmail.com>
2023-07-18 21:56:47 +0300

    Add temporary annotation

--- executor-service/src/main/java/executor/service/maintenance/plugin/proxy/ProxySourcesClientDecorator.java
+++ executor-service/src/main/java/executor/service/maintenance/plugin/proxy/ProxySourcesClientDecorator.java
@@ -2,11 +2,15 @@ package executor.service.maintenance.plugin.proxy;
 
 import executor.service.maintenance.proxy.ProxyValidationService;
 import executor.service.model.ProxyConfigHolderDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProxySourcesClientDecorator implements ProxySourcesClient {
 
     private final ProxySourcesClient client;
 
+    @Autowired
     public ProxySourcesClientDecorator(ProxySources sources) {
         client = new ProxyClient(sources);
     }
