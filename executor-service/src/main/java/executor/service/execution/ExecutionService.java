2068f75bd5efefb6ecfdb1a8fe7aa1ece1d313bd
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-08-11 23:49:27 +0300

    refactored

--- executor-service/src/main/java/executor/service/execution/ExecutionService.java
+++ executor-service/src/main/java/executor/service/execution/ExecutionService.java
@@ -1,11 +1,12 @@
 package executor.service.execution;
 
 import executor.service.execution.scenario.ScenarioExecutor;
-import executor.service.maintenance.ScenarioSourceListener;
+import executor.service.model.ScenarioDto;
+import executor.service.source.SourceListener;
 import org.openqa.selenium.WebDriver;
 
 public interface ExecutionService {
 
-    void execute(WebDriver webDriver, ScenarioSourceListener scenarioSourceListener,
+    void execute(WebDriver webDriver, SourceListener<ScenarioDto> scenarioSourceListener,
                  ScenarioExecutor scenarioExecutor);
 }
