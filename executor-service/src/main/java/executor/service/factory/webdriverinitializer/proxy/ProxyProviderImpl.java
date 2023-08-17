8e2b351f8a29e872eea59c051ae4b9ce716653f5
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-08-18 02:42:52 +0300

    refactored working with credentials

--- executor-service/src/main/java/executor/service/factory/webdriverinitializer/proxy/ProxyProviderImpl.java
+++ executor-service/src/main/java/executor/service/factory/webdriverinitializer/proxy/ProxyProviderImpl.java
@@ -3,9 +3,11 @@ package executor.service.factory.webdriverinitializer.proxy;
 import org.springframework.stereotype.Component;
 import executor.service.model.ProxyConfigHolderDto;
 import org.openqa.selenium.Proxy;
+
 @Component
 public class ProxyProviderImpl implements ProxyProvider {
     private static final String HTTP_PROXY_DELIMITER = ":";
+    private static final String AT_SIGN = "@";
     @Override
     public Proxy getProxy(ProxyConfigHolderDto proxyConfigHolder) {
         Proxy proxy = new Proxy();
@@ -13,8 +15,9 @@ public class ProxyProviderImpl implements ProxyProvider {
                 + HTTP_PROXY_DELIMITER
                 + proxyConfigHolder.getProxyNetworkConfig().getPort());
         if (proxyConfigHolder.getProxyCredentials() != null) {
-            proxy.setSocksUsername(proxyConfigHolder.getProxyCredentials().getUsername());
-            proxy.setSocksPassword(proxyConfigHolder.getProxyCredentials().getPassword());
+            String username = proxyConfigHolder.getProxyCredentials().getUsername();
+            String password = proxyConfigHolder.getProxyCredentials().getPassword();
+            proxy.setHttpProxy(username + HTTP_PROXY_DELIMITER + password + AT_SIGN + proxy.getHttpProxy());
         }
         return proxy;
     }
