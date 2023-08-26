292ead92b3c2ac46b22ef78eac7b8c051d8cf53b
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-08-27 02:58:02 +0300

    removed unused methods

--- executor-service/src/main/java/executor/service/exception/NoMoreProxiesException.java
+++
@@ -1,7 +0,0 @@
-package executor.service.exception;
-
-public class NoMoreProxiesException extends RuntimeException{
-    public NoMoreProxiesException() {
-        super("No more proxies");
-    }
-}
--- executor-service/src/main/java/executor/service/exception/ResourceFileNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package executor.service.exception;
-
-public class ResourceFileNotFoundException extends RuntimeException {
-    public ResourceFileNotFoundException(String resourceName) {
-        super(String.format("File %s not found in \"resources\" folder", resourceName));
-    }
-}
