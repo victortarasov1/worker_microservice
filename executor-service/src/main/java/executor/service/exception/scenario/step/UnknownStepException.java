3eb404837c7e86ac7d0af6601d0772ce8e9a6886
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-08-27 02:57:52 +0300

    removed unused constructor

--- executor-service/src/main/java/executor/service/exception/scenario/step/UnknownStepException.java
+++ executor-service/src/main/java/executor/service/exception/scenario/step/UnknownStepException.java
@@ -1,9 +1,6 @@
 package executor.service.exception.scenario.step;
 
-public class UnknownStepException extends StepExecutionException{
-    public UnknownStepException(String message, Throwable cause) {
-        super("Step with name: " + message +  " doesn`t exist!", cause);
-    }
+public class UnknownStepException extends StepExecutionException {
     public UnknownStepException(String message) {
         super(message);
     }
