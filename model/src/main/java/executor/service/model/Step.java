a1482ad05d8a175722270bab8852c63385948638
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-11-11 01:01:33 +0200

    added redis-hash

--- model/src/main/java/executor/service/model/Step.java
+++ model/src/main/java/executor/service/model/Step.java
@@ -1,6 +1,26 @@
 package executor.service.model;
 
-import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Reference;
+import org.springframework.data.redis.core.RedisHash;
 
-public record Step(UUID scenarioUUID, String action, String value) {
+@RedisHash("steps")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Step {
+    @Id
+    private String id;
+    @Reference
+    private StepReport report;
+    private String action;
+    private String value;
+
+    public Step(String action, String value) {
+        this.action = action;
+        this.value = value;
+    }
 }
