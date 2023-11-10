8e3b4da663ed27560a0264edafc6b0f4f13fe803
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-11-11 01:16:03 +0200

    created repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,3 +14,5 @@ include("redis:configuration")
 findProject(":redis:configuration")?.name = "configuration"
 include("redis:queue")
 findProject(":redis:queue")?.name = "queue"
+include("redis:repository")
+findProject(":redis:repository")?.name = "repository"
