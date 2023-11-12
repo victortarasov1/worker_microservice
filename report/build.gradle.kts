702d079f45e765fb20f783e66cbd92b3a53e9edf
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-11-13 00:11:23 +0200

    added spring data redis and :redis:repository deps

--- report/build.gradle.kts
+++ report/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation("org.aspectj:aspectjweaver:1.9.20.1")
     implementation(project(":model"))
-    implementation(project(":collection"))
     implementation(project(":execution"))
+    implementation(project(":redis:repository"))
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 }
