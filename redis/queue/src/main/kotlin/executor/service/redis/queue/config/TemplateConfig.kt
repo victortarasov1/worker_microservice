0055a2b5719254ed080d898df00eaf644b2f4745
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-11-13 02:34:43 +0200

    added object mapper bean

--- redis/queue/src/main/kotlin/executor/service/redis/queue/config/TemplateConfig.kt
+++ redis/queue/src/main/kotlin/executor/service/redis/queue/config/TemplateConfig.kt
@@ -1,11 +1,11 @@
 package executor.service.redis.queue.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
-
 @Configuration
 class TemplateConfig(private val jedisConnectionFactory: JedisConnectionFactory) {
     @Bean
@@ -15,4 +15,6 @@ class TemplateConfig(private val jedisConnectionFactory: JedisConnectionFactory)
         template.valueSerializer = GenericJackson2JsonRedisSerializer()
         return template
     }
+
+    @Bean fun objectMapper() = ObjectMapper()
 }
