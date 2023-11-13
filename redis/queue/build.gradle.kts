d99ecffe32fb06cc400164a7534b40e82310b49d
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-11-13 02:34:19 +0200

    added jackson dependency

--- redis/queue/build.gradle.kts
+++ redis/queue/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
     implementation(project(":redis:configuration"))
     implementation(project(":model"))
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.3")
 }
