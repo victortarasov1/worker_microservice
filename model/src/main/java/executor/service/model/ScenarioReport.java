4ad68831695f9b0685a0722af9ce5ec76cbbe71d
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-11-15 01:59:40 +0200

    removed redis specific annotations

--- model/src/main/java/executor/service/model/ScenarioReport.java
+++ model/src/main/java/executor/service/model/ScenarioReport.java
@@ -3,18 +3,13 @@ package executor.service.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.redis.core.RedisHash;
 
 import java.time.LocalDateTime;
 
-@RedisHash("scenario_reports")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ScenarioReport {
-    @Id
-    private String id;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
     private String errorMessage;
