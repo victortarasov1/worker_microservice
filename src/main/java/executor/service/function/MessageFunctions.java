630af4dd822f1af0b39f2eeb8e1063c3e55e23f8
victortarasov1 <victortarasov13032001@gmail.com>
2024-06-24 13:30:44 -0700

    updated image version

--- src/main/java/executor/service/function/MessageFunctions.java
+++ src/main/java/executor/service/function/MessageFunctions.java
@@ -4,7 +4,6 @@ import executor.service.factory.WebDriverProvider;
 import executor.service.model.Scenario;
 import executor.service.model.ScenarioReport;
 import executor.service.scenario.ScenarioExecutor;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -13,21 +12,17 @@ import org.springframework.context.annotation.Configuration;
 import java.util.function.Function;
 
 @Configuration
-@RequiredArgsConstructor
 public class MessageFunctions {
     private static final Logger logger = LoggerFactory.getLogger(MessageFunctions.class);
-    private final ScenarioExecutor executor;
-    private final WebDriverProvider provider;
 
     @Bean
-    public Function<Scenario, ScenarioReport> execute() {
+    public Function<Scenario, ScenarioReport> execute( ScenarioExecutor executor, WebDriverProvider provider) {
         return scenario -> {
             logger.info("Received scenario with ID: {}", scenario.getId());
             var webDriver = provider.create();
             var scenarioReport = executor.execute(scenario, webDriver);
             webDriver.quit();
             return scenarioReport;
-
         };
     }
 }
